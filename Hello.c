b2133369127dde180a390350eae61813742a1570
NikitaS148 <169018956+NikitaS148@users.noreply.github.com>
2024-05-06 10:33:21 +0530

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int a,b;
+    a=3, b=6;
+    int sum=a+b;
+    // printf(sum);
+
+    char str[] = "Hello World";
+    printf("%s\n",str);
+    
+    return 0;
+}
